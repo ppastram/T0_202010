f1d499ce7d4e8b8ce295564d23a4ebaa3a280dc6
ppastram <ppastram@hotmail.com>
2020-01-23 15:13:03 -0500

    Tratando de guardarlo

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -59,19 +59,41 @@ public class ArregloDinamico implements IArregloDinamico {
 
 		public String darElemento(int i) {
 			// TODO implementar
-			return null;
+			
+			return elementos[i];
 		}
 
 		public String buscar(String dato) {
 			// TODO implementar
 			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
+			
+			for ( int i = 0; i < elementos.length; i++)
+            {
+             	 if (elementos[i].compareTo(dato) == 0)
+             	 {
+             		 return dato;
+             	 }
+            } 
+			
 			return null;
 		}
 
 		public String eliminar(String dato) {
 			// TODO implementar
 			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-			return null;
+			
+			String [] copia = new String[elementos.length-1];
+			
+			for ( int i = 0; i < elementos.length; i++)
+            {
+             	 if (elementos[i].compareTo(dato) != 0)
+             	 {
+             		copia[i] = elementos[i];
+             	 }
+ 
+            }
+			
+			return dato;
 		}
 
 }
